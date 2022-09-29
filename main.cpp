80eedddbe49904f0cd1cd055ac25f213cb5eb192
Евгений Смирнов <e.smirnov@rczifort.local>
2022-09-29 10:17:41 +0300

    Change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char const *argv[])
 {
+    std::vector<int> vec{1, 2, 3};
+    for(auto elem : vec)
+    {
+        std::cout << elem << '\n';
+    }
+
     std::cout << "Hello world" << std::endl;
     return 0;
 }
